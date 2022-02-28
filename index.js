ca4e09cd0212685d36e173874edae51e73542ce2
Scarlett <lopezscarlett103096@yahoo.com>
2022-02-28 08:00:27 -0800

    commit tree 38aa25dfff03447cf8a453e0bb699f3616cd5297

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+//describe('index.js', function () {
+    //const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(driver1,driver2) {
+    return [driver1 = 'Antonia', driver2 =  'Nuru'];
+}
+
+function returnLastTwoDrivers(driver3,driver4) {
+    return [driver3 = 'Amari', driver4 = 'Mo'];
+}
+
+selectingDrivers = driver => [returnFirstTwoDrivers(), returnLastTwoDrivers()];
